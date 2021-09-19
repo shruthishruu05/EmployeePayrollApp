2e2500a2fd45087ba23a48f5cdd3935bdea497ef
shruthishruu05 <shruthi.m@perfios.com>
2021-09-19 19:21:46 +0530

    REFACTORED : reading from a file

--- lib/src/test/java/com/bridgelabz/employeepayrollapp/EmployeePayrollServiceTest.java
+++ lib/src/test/java/com/bridgelabz/employeepayrollapp/EmployeePayrollServiceTest.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.employeepayrollapp;
 
-import java.util.Arrays; 
+import java.util.Arrays;
+
+import org.junit.Assert;
 import org.junit.Test;
 import com.bridgelabz.employeepayrollapp.EmployeePayrollService.IOService;
 
@@ -19,5 +21,18 @@ public class EmployeePayrollServiceTest
 		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
 		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
 		
+		employeePayrollService.printData(IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
+		
 	}
+	
+	@Test
+	public void givenFile_WhenRead_ShouldReturnNumberOfEntries() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		long entries = employeePayrollService.readDataFromFile(IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
+	}
+		
+	
 }
