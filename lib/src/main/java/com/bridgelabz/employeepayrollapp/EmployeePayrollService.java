b5eccf7dd760d65a98799af1ed89195fc15e882f
shruthishruu05 <shruthi.m@perfios.com>
2021-09-19 15:26:07 +0530

    ADDED : contains method to read and write employee payroll to a console

--- lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollService.java
+++ lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollService.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class EmployeePayrollService 
-{
+public class EmployeePayrollService {
 	
 	public enum IOService {
 		CONSOLE_IO, FILE_IO, DB_IO, REST_IO;
