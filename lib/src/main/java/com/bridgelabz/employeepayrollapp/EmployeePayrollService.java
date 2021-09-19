85fe8394647fd94729e382a6325e5d3187ed980b
shruthishruu05 <shruthi.m@perfios.com>
2021-09-19 17:40:56 +0530

    REFACTORED : Contains method to write a file

--- lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollService.java
+++ lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollService.java
@@ -30,9 +30,12 @@ public class EmployeePayrollService {
 		double salary = consoleInputReader.nextDouble();
 		employeePayrollList.add(new EmployeePayrollData(id, empName, salary));
 	}
-	private void writeEmployeePayrollData() 
-	{
-		System.out.println("\nWriting Employee Payroll to Console\n" + employeePayrollList);
+	public void writeEmployeePayrollData(IOService ioService) {
+		if(ioService.equals(IOService.CONSOLE_IO))
+			System.out.println("\nWriting Employee Payroll Roster to Console\n" + employeePayrollList);
+		
+		else if(ioService.equals(IOService.FILE_IO))
+			new EmployeePayrollFileIOService().writeData(employeePayrollList);
 	}
 	
 	public static void main(String [] args)
@@ -43,7 +46,7 @@ public class EmployeePayrollService {
 		Scanner consoleInputReader = new Scanner(System.in);
 		
 		employeePayrollService.readEmployeePayrollData(consoleInputReader);
-		employeePayrollService.writeEmployeePayrollData();
+		employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
 		
 	
 	}
