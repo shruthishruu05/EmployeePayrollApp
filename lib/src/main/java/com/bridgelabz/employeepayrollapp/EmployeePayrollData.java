3501dd8fc541c72a5b38f96dc5eeb13da504e080
shruthishruu05 <shruthi.m@perfios.com>
2021-09-26 10:07:15 +0530

    REFACTORED : Added new constructor for start date

--- lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollData.java
+++ lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollData.java
@@ -1,15 +1,26 @@
 package com.bridgelabz.employeepayrollapp;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
 	int employeeId;
 	String employeeName;
 	double employeeSalary;
+	private LocalDate startDate;
+	
 	
 	public EmployeePayrollData(int employeeId, String employeeName, Double employeeSalary) {
 		super();
 		this.employeeId = employeeId;
 		this.employeeName = employeeName;
 		this.employeeSalary = employeeSalary;
+	}
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+		this.employeeId = id;
+		this.employeeName = name;
+		this.employeeSalary = salary;
+		this.startDate = startDate;
+
 	}
 	@Override
 	public String toString() {
