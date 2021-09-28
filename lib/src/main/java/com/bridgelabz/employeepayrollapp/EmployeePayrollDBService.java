fc0c3a1218f9248af880df9f2b6dfda2c15bd211
shruthishruu05 <shruthi.m@perfios.com>
2021-09-28 14:58:06 +0530

    REFACTORED : insert method to add  employee details to database

--- lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollDBService.java
+++ lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollDBService.java
@@ -2,285 +2,288 @@ package com.bridgelabz.employeepayrollapp;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import com.bridgelabz.employeepayrollapp.EmployeePayrollException.ExceptionType;
+
 import com.mysql.jdbc.Connection;
 import java.sql.*;
 
 public class EmployeePayrollDBService {
-	
+
 	private PreparedStatement employeePayrollDataStatement;
 	private static EmployeePayrollDBService employeePayrollDBService;
-	private static List<EmployeePayrollData> employeePayrollList;
+	private EmployeePayrollDBService() {}
+	
 	public static EmployeePayrollDBService getInstance() {
-		if(employeePayrollDBService == null)
+		if(employeePayrollDBService == null) {
 			employeePayrollDBService = new EmployeePayrollDBService();
+		}
 		return employeePayrollDBService;
 	}
 	
-	public void displayDate() {
-		System.out.println(employeePayrollList);
-	}
-	private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet) {
-		
-		employeePayrollList = new ArrayList<>();
-		
-		try {
-			while(resultSet.next()) {
-				int id = resultSet.getInt("id");
-				String name = resultSet.getString("name");
-				double basicSalary = resultSet.getDouble("salary");
-				LocalDate startDate = resultSet.getDate("start").toLocalDate();
-				employeePayrollList.add(new EmployeePayrollData(id, name, basicSalary, startDate));
-				
-			}
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return employeePayrollList;
-		
-	}
-	
-	public List<EmployeePayrollData> getEmployeePayrollData(String name) {
-		
-		List<EmployeePayrollData> employeePayrollList = null;
-		if(this.employeePayrollDataStatement == null)
-			this.prepareStatementForEmployeeData();
-		try {
-			employeePayrollDataStatement.setString(1,name);
-			ResultSet resultSet = employeePayrollDataStatement.executeQuery();
-			employeePayrollList = this.getEmployeePayrollData(resultSet);	
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return employeePayrollList;
-	}
-
-	
 	public List<EmployeePayrollData> readData() {
-		String sql = "SELECT * FROM employee_payroll"; 
+		String sql = "SELECT * FROM employee_payroll";
 		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 		try {
 			Connection connection = this.getConnection();
 			Statement statement = connection.createStatement();
 			ResultSet result = statement.executeQuery(sql);
-			while(result.next()) {
-				int id = result.getInt("id");
-				String name = result.getString("id");
-				double salary = result.getDouble("salary");
-				LocalDate startDate = result.getDate("start").toLocalDate();
-				employeePayrollList.add(new EmployeePayrollData(id, name, salary,startDate));
-				
-			}
+			employeePayrollList = this.getEmployeePayrollData(result);
 			connection.close();
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return employeePayrollList;
-	}
-
-	public Connection getConnection() throws SQLException
-	{
-		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?userSSL=false";
-		String userName = "root";
-		String password = "Welcome@123";
-		Connection connection;
-		System.out.println("Connecting to database:"+jdbcURL);
-		connection =  (Connection) DriverManager.getConnection(jdbcURL,userName,password);
-		System.out.println("Connection is successful!!!!"+connection);
-		return connection;
-	
-	}
-	
-	public int updateEmployeeSalary(String name, double salary) throws SQLException {
-		return this.updateEmployeeDataUsingStatement(name,salary);
-		
-	}
-	
-	int updateEmployeeDataUsingStatement(String name,double salary) throws SQLException {
-		String sqlString = String.format("update employee_payroll set salary = %2f where name = '%s';",salary,name);
-		try(Connection connection = this.getConnection()) {
-			Statement statement = connection.createStatement();
-			return statement.executeUpdate(sqlString);
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return 0;
-		
-	}
-	private void prepareStatementForEmployeeData() {
-			
-			try {
-				Connection connection = this.getConnection();
-				String sqlStatement = "SELECT * FROM employee_payroll WHERE name = ?;";
-				employeePayrollDataStatement = connection.prepareStatement(sqlStatement);
-			}
-			catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-	public List<EmployeePayrollData> getEmployeeDetailsBasedOnNameUsingStatement(String name) {
-		String sqlStatement = String.format("SELECT * FROM employee_payroll WHERE name = '%s';",name);
-		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-				
-		try (Connection connection = getConnection();){
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(sqlStatement);
-			employeePayrollList = this.getEmployeePayrollData(resultSet);
-		}
-		catch(SQLException e){
-			e.printStackTrace();
-		}
-		return employeePayrollList;
-	}
-	
-	public List<EmployeePayrollData> getEmployeeDetailsBasedOnStartDateUsingStatement(String startDate) {
-			
-			String sqlStatement = String.format("SELECT * FROM employee_payroll WHERE start BETWEEN CAST('%s' AS DATE) AND DATE(NOW());",startDate);
-			List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-					
-			try (Connection connection = getConnection();){
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(sqlStatement);
-				employeePayrollList = this.getEmployeePayrollData(resultSet);
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-			}
-			return employeePayrollList;
-		}
-
-	public List<EmployeePayrollData> getEmployeeDetailsBasedOnStartDateUsingPreparedStatement(String startDate) {
-		List<EmployeePayrollData> employeePayrollList = null;
-		if(this.employeePayrollDataStatement == null)
-			this.preparedStatementForEmployeeDataBasedOnStartDate();
-		try {
-			employeePayrollDataStatement.setString(1,startDate);
-			ResultSet resultSet = employeePayrollDataStatement.executeQuery();
-			employeePayrollList = this.getEmployeePayrollData(resultSet);	
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
 		return employeePayrollList;
 	}
-
-		private void preparedStatementForEmployeeDataBasedOnStartDate() {
-			
+	
+	
+	public Connection getConnection() throws EmployeePayrollException
+	{
+		Connection connection;
+		try {
+			String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?userSSL=false";
+			String userName = "root";
+			String password = "Welcome@123";
+			System.out.println("Connecting to database:"+jdbcURL);
+			connection =  (Connection) DriverManager.getConnection(jdbcURL,userName,password);
+			System.out.println("Connection is successful!!!!"+connection);
+		}
+		catch(SQLException e) {
+			throw new EmployeePayrollException(EmployeePayrollException.ExceptionType.FAILED_TO_CONNECT, "Failed to connect to database");
+		}
+		return connection;
+	
+	}
+	
+	public int updateEmployeeSalary(String name, double salary) throws EmployeePayrollException {
+		return this.updateEmployeeDataUsingStatement(name,salary);
+	}
+	
+	private int updateEmployeeDataUsingStatement(String name,double salary) {
+		String sqlString = String.format("update employee_payroll set salary = %2f where name = '%s';",salary,name);
+		try(Connection connection = this.getConnection()) {
+			Statement statement = connection.createStatement();
+			return statement.executeUpdate(sqlString);
+		
+		}
+		catch(SQLException e) {
+			throw new EmployeePayrollException(EmployeePayrollException.ExceptionType.UPDATE_FAILED, "Failed to update the given data");
+		}
+	
+	}
+	
+	public EmployeePayrollData addEmployeeToPayroll(String name, Double salary, LocalDate startDate, char gender) {
+		int employeeID = -1;
+		EmployeePayrollData employeePayrollData = null;
+		String sql = String.format("INSERT INTO employee_payroll(name,gender,salary,start)VALUES('%s','%s','%2f','%s')",name,gender,
+				salary,startDate.toString());
+		try {
+			Connection connection = this.getConnection();
+			Statement statement = connection.createStatement();
+			int result = statement.executeUpdate(sql,statement.RETURN_GENERATED_KEYS);
+			if(result == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if(resultSet.next()) employeeID = resultSet.getInt(1);
+			}
+			connection.close();
+			employeePayrollData = new EmployeePayrollData(employeeID, name, gender,salary, startDate);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(employeePayrollData);
+		return employeePayrollData;
+	}
+	
+	public void writeDB(List<EmployeePayrollData> employees) {
+		employees.stream().forEach(employee ->{
+			String sql = String.format("INSERT INTO employee_payroll(name,gender,salary,start)VALUES('%s','%s','%2f','%s')",employee.name,employee.gender,
+										employee.salary,employee.startDate.toString());
 			try {
 				Connection connection = this.getConnection();
-				String sqlStatement = "SELECT * FROM employee_payroll WHERE start BETWEEN CAST(? AS DATE) AND DATE(NOW());";
-				employeePayrollDataStatement = connection.prepareStatement(sqlStatement);
+				Statement statement = connection.createStatement();
+				int result = statement.executeUpdate(sql);
+				connection.close();
 			}
 			catch(SQLException e) {
 				e.printStackTrace();
 			}
-		}
+		});
 		
-		public List<Double> getSumOfSalaryBasedOnGenderUsingStatement() {
-			
-			String sqlStatement = "SELECT gender, SUM(salary) AS TotalSalary FROM employee_payroll GROUP BY gender;";
-			List<Double> sumOfSalaryBasedOnGender = new ArrayList<>();
-					
-			try (Connection connection = getConnection();){
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(sqlStatement);
-				while(resultSet.next()) {
-					double salary = resultSet.getDouble("TotalSalary");
-					sumOfSalaryBasedOnGender.add(salary);
-				}
-			}
-			catch(SQLException e){
-				throw new EmployeePayrollException(ExceptionType.CANNOT_EXECUTE_QUERY, "Could not Execute Query! Check the Syntax");
-			}
-			return sumOfSalaryBasedOnGender;
-		}
-		
-		public List<Double> getAverageOfSalaryBasedOnGenderUsingStatement() {
-			
-			String sqlStatement = "SELECT gender, AVG(salary) AS AverageSalary FROM employee_payroll GROUP BY gender;";
-			List<Double> averageOfSalaryBasedOnGender = new ArrayList<>();
-					
-			try (Connection connection = getConnection();){
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(sqlStatement);
-				while(resultSet.next()) {
-					double salary = resultSet.getDouble("AverageSalary");
-					averageOfSalaryBasedOnGender.add(salary);
-				}
-			}
-			catch(SQLException e){
-				throw new EmployeePayrollException(ExceptionType.CANNOT_EXECUTE_QUERY, "Could not Execute Query! Check the Syntax");
-			}
-			return averageOfSalaryBasedOnGender;
-		}
-		
-		public List<Double> getMinimumSalaryBasedOnGenderUsingStatement() {
-			
-			String sqlStatement = "SELECT gender, MIN(salary) AS MinimumSalary FROM employee_payroll GROUP BY gender;";
-			List<Double> MinimumSalaryBasedOnGender = new ArrayList<>();
-					
-			try (Connection connection = getConnection();){
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(sqlStatement);
-				while(resultSet.next()) {
-					double salary = resultSet.getDouble("MinimumSalary");
-					MinimumSalaryBasedOnGender.add(salary);
-				}
-			}
-			catch(SQLException e){
-				throw new EmployeePayrollException(ExceptionType.CANNOT_EXECUTE_QUERY, "Could not Execute Query! Check the Syntax");
-
-			}
-			return MinimumSalaryBasedOnGender;
-		}
-		
-		public List<Double> getMaximumSalaryBasedOnGenderUsingStatement() {
-			
-			String sqlStatement = "SELECT gender, MAX(salary) AS MaximumSalary FROM employee_payroll GROUP BY gender;";
-			List<Double> MaximumSalaryBasedOnGender = new ArrayList<>();
-					
-			try (Connection connection = getConnection();){
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(sqlStatement);
-				while(resultSet.next()) {
-					double salary = resultSet.getDouble("MaximumSalary");
-					MaximumSalaryBasedOnGender.add(salary);
-				}
-			}
-			catch(SQLException e){
-				throw new EmployeePayrollException(ExceptionType.CANNOT_EXECUTE_QUERY, "Could not Execute Query! Check the Syntax");
-
-			}
-			return MaximumSalaryBasedOnGender;
-		}
-
-		public List<Integer> getCountOfEmployeesBasedOnGenderUsingStatement() {
-			
-			String sqlStatement = "SELECT gender, COUNT(gender) AS CountBasedOnGender FROM employee_payroll GROUP BY gender;";
-			List<Integer> CountBasedOnGender = new ArrayList<>();
-					
-			try (Connection connection = getConnection();){
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(sqlStatement);
-				while(resultSet.next()) {
-					int count = resultSet.getInt("CountBasedOnGender");
-					CountBasedOnGender.add(count);
-				}
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-			}
-			return CountBasedOnGender;
-		}
-		
-		
-	
 	}
 	
+	public int countEntries() {
+		String sql = "SELECT * FROM employee_payroll";
+		int count  =0;
+		try {
+			Connection connection = this.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				count++;
+			}
+			connection.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
 
+	
+	
+	private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet) {
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		
+		try {
+			while(resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				char gender = resultSet.getString("gender").charAt(0);
+				double basicSalary = resultSet.getDouble("salary");
+				LocalDate startDate = resultSet.getDate("start").toLocalDate();
+				employeePayrollList.add(new EmployeePayrollData(id, name, gender,basicSalary, startDate));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollList;
+	}
+	public List<EmployeePayrollData> getEmployeePayrollData(String name) {
+		List<EmployeePayrollData> employeePayrollDataList = null;
+		if(this.employeePayrollDataStatement == null) {
+			this.prepareStatementForEmployeeData();
+		}
+		try {
+			employeePayrollDataStatement.setString(1, name);
+			ResultSet resultSet = employeePayrollDataStatement.executeQuery();
+			employeePayrollDataList = this.getEmployeePayrollData(resultSet);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollDataList;
+	}
+
+	private void prepareStatementForEmployeeData() {
+		try {
+			Connection connection = this.getConnection();
+			String sqlStatement = "select * from employee_payroll where name = ?;";
+			employeePayrollDataStatement = connection.prepareStatement(sqlStatement);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<EmployeePayrollData> getEmployeesInDateRange(String date1, String date2) {
+		List<EmployeePayrollData> employeePayrollList = null;
+		String sql = String.format("select * from employee_payroll where start between cast('%s' as date) and cast('%s' as date)",date1,date2);
+		try {
+			Connection connection = this.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			employeePayrollList = this.getEmployeePayrollData(result);
+			connection.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return employeePayrollList;
+	}
+	
+	public HashMap<Character, Double> getGenderWiseTotalSalary() throws EmployeePayrollException{
+		HashMap<Character,Double> salaryMap = new HashMap<>();
+		String sql = "SELECT gender , SUM(salary) as 'SUM'  FROM employee_payroll GROUP BY gender;";
+		try(Connection connection = this.getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				char key = result.getString("gender").charAt(0);
+				double value = result.getDouble("SUM");
+				salaryMap.put(key, value);
+			}
+		}
+		catch (SQLException e) {
+			throw new EmployeePayrollException(EmployeePayrollException.ExceptionType.CANNOT_EXECUTE_QUERY, "Cannot execute the query");
+		}	
+		return salaryMap;
+	}
+	
+	public HashMap<Character, Double> getGenderWiseMinSalary() throws EmployeePayrollException{
+		HashMap<Character,Double> salaryMap = new HashMap<>();
+		String sql = "SELECT gender , MIN(salary) as 'MIN'  FROM employee_payroll GROUP BY gender;";
+		try(Connection connection = this.getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				char key = result.getString("gender").charAt(0);
+				double value = result.getDouble("MIN");
+				salaryMap.put(key, value);
+			}
+		}
+		catch (SQLException e) {
+			throw new EmployeePayrollException(EmployeePayrollException.ExceptionType.CANNOT_EXECUTE_QUERY, "Cannot execute the query");
+		}	
+		return salaryMap;
+	}
+	
+	public HashMap<Character, Double> getGenderWiseMaxSalary() throws EmployeePayrollException{
+		HashMap<Character,Double> salaryMap = new HashMap<>();
+		String sql = "SELECT gender , MAX(salary) as 'MAX'  FROM employee_payroll GROUP BY gender;";
+		try(Connection connection = this.getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				char key = result.getString("gender").charAt(0);
+				double value = result.getDouble("MAX");
+				salaryMap.put(key, value);
+			}
+		}
+		catch (SQLException e) {
+			throw new EmployeePayrollException(EmployeePayrollException.ExceptionType.CANNOT_EXECUTE_QUERY, "Cannot execute the query");
+		}	
+		return salaryMap;
+	}
+	
+	public HashMap<Character, Double> getGenderWiseAvgSalary() throws EmployeePayrollException{
+		HashMap<Character,Double> salaryMap = new HashMap<>();
+		String sql = "SELECT gender , AVG(salary) as 'AVG'  FROM employee_payroll GROUP BY gender;";
+		try(Connection connection = this.getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				char key = result.getString("gender").charAt(0);
+				double value = result.getDouble("Avg");
+				salaryMap.put(key, value);
+			}
+		}
+		catch (SQLException e) {
+			throw new EmployeePayrollException(EmployeePayrollException.ExceptionType.CANNOT_EXECUTE_QUERY, "Cannot execute the query");
+		}	
+		return salaryMap;
+	}
+	
+	public HashMap<Character, Integer> getGenderWiseCount() throws EmployeePayrollException{
+		HashMap<Character,Integer> countMap = new HashMap<>();
+		String sql = "SELECT gender , COUNT(salary) as 'COUNT'  FROM employee_payroll GROUP BY gender;";
+		try(Connection connection = this.getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while(result.next()) {
+				char key = result.getString("gender").charAt(0);
+				int value = result.getInt("COUNT");
+				countMap.put(key, value);
+			}
+		}
+		catch (SQLException e) {
+			throw new EmployeePayrollException(EmployeePayrollException.ExceptionType.CANNOT_EXECUTE_QUERY, "Cannot execute the query");
+		}	
+		return countMap;
+	}
+
+}
