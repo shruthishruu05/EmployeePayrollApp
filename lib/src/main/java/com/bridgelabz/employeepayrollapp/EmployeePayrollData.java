743471d3f6401756ede6f5db50e15f6bf3a22540
shruthishruu05 <shruthi.m@perfios.com>
2021-09-28 15:09:43 +0530

    REFACTORED : constructor

--- lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollData.java
+++ lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollData.java
@@ -3,27 +3,34 @@ package com.bridgelabz.employeepayrollapp;
 import java.time.LocalDate;
 
 public class EmployeePayrollData {
-	int employeeId;
-	String employeeName;
-	double employeeSalary;
-	private LocalDate startDate;
+	public int id;
+	public String name;
+	public char gender;
+	public double salary;
+	public LocalDate startDate;
 	
-	public EmployeePayrollData(int employeeId, String employeeName, Double employeeSalary) {
-		super();
-		this.employeeId = employeeId;
-		this.employeeName = employeeName;
-		this.employeeSalary = employeeSalary;
+	public EmployeePayrollData(Integer id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary =salary;
 	}
-	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
-		this.employeeId = id;
-		this.employeeName = name;
-		this.employeeSalary = salary;
+	public EmployeePayrollData(Integer id, String name, char gender,  double salary, LocalDate startDate) {
+		this(id,name,salary);
+		this.gender = gender;
 		this.startDate = startDate;
-
 	}
 	@Override
 	public String toString() {
-		return "EmployeePayrollData [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeSalary="
-				+ employeeSalary + "]";
+		return "id: "+this.id+" name: "+this.name+" salary: "+this.salary;
 	}
+	
+	@Override
+	public boolean equals( Object obj) {
+		if(obj == this) return true;
+		if(obj == null || getClass() != obj.getClass()) return false;
+		
+		EmployeePayrollData that = (EmployeePayrollData) obj;
+		return this.id == that.id && this.name.equals(that.name) && Double.compare(that.salary, this.salary) ==0;
+	}
+	
 }
