85f7e3d9b86b08df9efd245b97d5df57d65a0417
shruthishruu05 <shruthi.m@perfios.com>
2021-09-28 15:10:46 +0530

    REFACTORED : custom exception

--- lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollException.java
+++ lib/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollException.java
@@ -2,7 +2,7 @@ package com.bridgelabz.employeepayrollapp;
 
 public class EmployeePayrollException extends RuntimeException {
 	enum ExceptionType {
-        CONNECTION_FAIL, CANNOT_EXECUTE_QUERY, UPDATE_FAILED
+        FAILED_TO_CONNECT, CANNOT_EXECUTE_QUERY, UPDATE_FAILED
     }
 
     ExceptionType exceptionType;
